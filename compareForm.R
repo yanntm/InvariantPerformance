ce7d3fa86c213ab95a33b04e11aa7497fc447c3e
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2023-03-29 16:01:51 +0200

    Add files via upload

---
+++ compareForm.R
@@ -0,0 +1,75 @@
+
+
+library(dplyr,lib='~/usr/localR/library')
+library(ggplot2,lib='~/usr/localR/library')
+library(ggbeeswarm,lib='~/usr/localR/library')
+
+data <- read.csv(file = "invar.csv",  dec = ".", sep = ",",  header = TRUE, stringsAsFactors = FALSE)
+
+fvA <- subset(data, Tool=="itstools")
+fvB <- subset(data, Tool=="tina")
+
+colnames(fvA) <- paste(colnames(fvA), "its", sep = "_")
+colnames(fvB) <- paste(colnames(fvB), "tina", sep = "_")
+
+fvA <- dplyr::rename(fvA, Model = Model_its)
+fvB <- dplyr::rename(fvB, Model = Model_tina)
+
+fperf = inner_join(fvA, fvB)
+
+fperf <- mutate(fperf, verdict.color = ifelse(
+  Status_its == "OK" & Status_tina != "OK","ITS wins", ifelse(Status_tina == "OK" & Status_its != "OK","Tina wins", "equal")));
+
+# fperf <- mutate(fperf, techAgree = ifelse(ConstP_its + NBP_its == NBP_tina,"same", "different"));
+
+fperf <- subset(fperf, grepl("-PT-",Model,fixed=TRUE))
+
+
+timereal <- ggplot(fperf, 
+                   aes(x=Time_its, y=Time_tina, color=verdict.color)) + 
+  geom_point() + 
+  scale_y_continuous(trans='log10', labels = function(x) format(x, scientific = FALSE)) +
+  scale_x_continuous(trans='log10', labels = function(x) format(x, scientific = FALSE))  + 
+  geom_abline(intercept = 0, slope = 1)  + 
+  scale_color_manual(values = c("orange", "blue","black")) +
+  scale_fill_hue(l=45) +
+#  annotate("text", x=1200, y=10, label= nrow(subset(perf, Test.fin_vA < Test.fin_vB))) + 
+#  annotate("text", x=10, y=1200, label= nrow(subset(perf, Test.fin_vA > Test.fin_vB))) + 
+  guides(color=guide_legend(element_blank())) +  
+  theme(legend.position = c(0.92, 0.05)) +
+  xlab("Time (in msec.) for ITS") +
+  ylab("Time (in msec.) for Tina") +
+  ggtitle("Run time in mseconds as measured by time command")
+
+fperf <- mutate(fperf, RepTime_its = ifelse(
+  Status_its == "OK",PTime_its+TTime_its, 120000));
+
+fperf <- mutate(fperf, RepTime_tina = ifelse(
+  Status_tina == "OK",PTime_tina+TTime_tina, 120000));
+
+timereal2 <- ggplot(fperf, 
+                   aes(x=RepTime_its, y=RepTime_tina, color=verdict.color)) + 
+  geom_point() + 
+  scale_y_continuous(trans='log10', labels = function(x) format(x, scientific = FALSE)) +
+  scale_x_continuous(trans='log10', labels = function(x) format(x, scientific = FALSE))  + 
+  geom_abline(intercept = 0, slope = 1)  + 
+  scale_color_manual(values = c("orange", "blue","black")) +
+  scale_fill_hue(l=45) +
+  #  annotate("text", x=1200, y=10, label= nrow(subset(perf, Test.fin_vA < Test.fin_vB))) + 
+  #  annotate("text", x=10, y=1200, label= nrow(subset(perf, Test.fin_vA > Test.fin_vB))) + 
+  guides(color=guide_legend(element_blank())) +  
+  theme(legend.position = c(0.92, 0.05)) +
+  xlab("Time (in msec.) for ITS") +
+  ylab("Time (in msec.) for Tina") +
+  ggtitle("Run time in mseconds for computing PFlow+TFlow as reported by tools")
+
+
+
+#perf$deltatime<-perf$duration.ms._vA-perf$duration.ms._vB
+#perf$deltatest<-perf$Test.fin_vA-perf$Test.fin_vB
+
+pdf(file = "fplots.pdf")    #paper = "a4r"
+# print(timereal)
+print(timereal2)
+dev.off()
+
---
+++ logs2csv.pl
@@ -0,0 +1,130 @@
+#! /usr/bin/perl
+
+
+use strict vars;
+
+
+print "Model,Tool,CardP,CardT,CardA,PTime,TTime,ConstP,NBP,NBT,TotalTime,Time,Mem,Status\n";
+
+my @files = <*its>;
+#print "working on files : @files";
+foreach my $file (@files) {
+    if ( $file =~ /its$/ ) {	
+	#print "looking at file : $file";
+	my $model=$file;
+	$model =~ s/\.its//g ;
+	my $tool="itstools";
+	my $status="UNK";
+	my $ptime=-1, my $ttime=-1, my $constp=0, my $nbp=0, my $nbt=0, my $tottime=-1, my $tmem=-1;
+	my $timecmd=-1;
+	my $colp=-1,my $colt=-1;
+	my $cardp=-1,my $cardt=-1,my$carda=-1;
+	open IN, "< $file";
+	while (my $line=<IN>) {
+	    chomp $line;
+	    if ($line =~ /Reduce places removed (\d+) places/) {
+		$constp=$1;
+		next;
+	    } elsif ($line =~ /Computed (\d+) P\s+flows in (\d+) ms/) {
+		$nbp=$1;
+		$ptime=$2;
+		next;
+	    } elsif ($line =~ /Computed (\d+) T\s+flows in (\d+) ms/) {
+		$nbt=$1;
+		$ttime=$2;
+		next;
+	    } elsif ($line =~ /Parsed PT model containing (\d+) places and (\d+) transitions and (\d+) arcs in (\d+) ms/) {
+		$cardp=$1;
+		$cardt=$2;
+		$carda=$3;
+		# $parsetime=$4;
+	    } elsif ($line =~ /Unfolded HLPN to a Petri net with (\d+) places and (\d+) transitions (\d+) arcs in (\d+) ms/) {
+		$cardp=$1;
+		$cardt=$2;
+		$carda=$3;
+		# $parsetime=$4;
+	    } elsif ($line =~ /Total runtime (\d+) ms/) {
+		$tottime=$1;
+		$status="OK";
+		next;
+	    } elsif ($line =~ /TIME LIMIT/) {
+		$tottime=120000;
+		$status="TO";
+		next;
+	    } elsif ($line =~ /.*user .*system (.*)elapsed .*CPU \(.*avgtext+.*avgdata (.*)maxresident\)k/) {
+		$timecmd=$1;
+		if ($timecmd =~ /(\d+):(\d+)\.(\d+)/) {
+		    $timecmd = 60000*$1 + $2*1000 + $3;
+		}
+		$tmem=$2;
+	    }
+	}	
+	close IN;
+	print "$model,itstools,$cardp,$cardt,$carda,$ptime,$ttime,$constp,$nbp,$nbt,$tottime,$timecmd,$tmem,$status\n";
+    }
+}   
+
+
+
+@files = <*struct>;
+#print "working on files : @files";
+foreach my $file (@files) {
+    if ( $file =~ /struct$/ ) {	
+	#print "looking at file : $file";
+	my $model=$file;
+	$model =~ s/\.struct//g ;
+	my $tool="tina";
+	my $status="UNK";
+	my $ptime=-1, my $ttime=-1, my $constp=0, my $nbp=-1, my $nbt=-1, my $tottime=-1, my $tmem=-1;
+	my $timecmd=-1;
+	my $colp=-1,my $colt=-1;
+	my $cardp=-1,my $cardt=-1,my$carda=-1;
+	open IN, "< $file";
+	while (my $line=<IN>) {
+	    chomp $line;
+	    if ($line =~ /(\d+) places, (\d+) transitions, (\d+) arcs/) {
+		$cardp=$1;
+		$cardt=$2;
+		$carda=$3;
+		# $parsetime=$4;
+	    } elsif ($line =~ /(\d+) flow\(s\)/) {
+		if ($nbp == -1) {
+		    $nbp=$1;
+		} else {
+		    $nbt=$1;
+		}
+	    } elsif ($line =~ /^(\d+\.\d+)s$/) {
+		if ($nbp == -1) {
+		    next;
+		} elsif ($nbp != -1 && $nbt == -1) {
+		    $ptime=$1*1000;
+		} elsif ($nbp != -1 && $nbt != -1) {
+		    $ttime=$1*1000;
+		    $status="OK";
+		}
+		next;
+	    } elsif ($line =~ /TIME LIMIT/) {
+		$timecmd=120000;
+		$status="TO";
+		next;
+	    } elsif ($line =~ /Command terminated by signal 9/) {
+		$status="MOVF";
+		next;
+	    } elsif ($line =~ /Command exited with non-zero status/) {
+		$status="ERR";
+		next;
+	    } elsif ($line =~ /.*user .*system (.*)elapsed .*CPU \(.*avgtext+.*avgdata (.*)maxresident\)k/) {
+		$timecmd=$1;
+		if ($timecmd =~ /(\d+):(\d+)\.(\d+)/) {
+		    $timecmd = 60000*$1 + $2*1000 + $3;
+		}
+		$tmem=$2;
+	    } else {
+	#	print "nomatch: $line\n";
+	    }
+	}	
+	close IN;
+	print "$model,tina,$cardp,$cardt,$carda,$ptime,$ttime,$constp,$nbp,$nbt,$tottime,$timecmd,$tmem,$status\n";
+    }
+}   
+
