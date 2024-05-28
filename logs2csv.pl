12e556a9e101dfca7879c16c7cdd8e3a542f1f62
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2024-05-28 17:41:59 +0200

    grab the logs from wider versions too

--- logs2csv.pl
+++ logs2csv.pl
@@ -6,71 +6,70 @@ use strict vars;
 
 print "Model,Tool,CardP,CardT,CardA,PTime,TTime,ConstP,NBP,NBT,TotalTime,Time,Mem,Status\n";
 
-my @files = <*petri>;
-#print "working on files : @files";
+my @files = <*petri*>;
 foreach my $file (@files) {
-    if ( $file =~ /petri$/ ) {	
-	#print "looking at file : $file";
-	my $model=$file;
-	$model =~ s/\.petri//g ;
-	my $tool="PetriSpot";
-	my $status="UNK";
-	my $ptime=-1, my $ttime=-1, my $constp=0, my $nbp=0, my $nbt=0, my $tottime=-1, my $tmem=-1;
-	my $timecmd=-1;
-	my $colp=-1,my $colt=-1;
-	my $cardp=-1,my $cardt=-1,my$carda=-1;
-	open IN, "< $file";
-	my $ofp=0, my $oft=0;
-	while (my $line=<IN>) {
-	    chomp $line;
-	    if ($line =~ /Reduce places removed (\d+) places/) {
-		$constp=$1;
-		next;
-	    } elsif ($line =~ /Computed (\d+) P\s+flows in (\d+) ms/) {
-		$nbp=$1;
-		$ptime=$2;
-		next;
-	    } elsif ($line =~ /Computed (\d+) T\s+flows in (\d+) ms/) {
-		$nbt=$1;
-		$ttime=$2;
-		next;
-	    } elsif ($line =~ /Invariants computation overflowed/) {
-		if ($ptime == -1) {
-		    $ofp=1;
-		} else {
-		    $oft=1;
-		}
-	    } elsif ($line =~ /Parsed PT model containing (\d+) places and (\d+) transitions and (\d+) arcs in (\d+) ms/) {
-		$cardp=$1;
-		$cardt=$2;
-		$carda=$3;
-		# $parsetime=$4;
-	    } elsif ($line =~ /Total runtime (\d+) ms/) {
-		$tottime=$1;
-		$status="OK";
-		next;
-	    } elsif ($line =~ /TIME LIMIT/) {
-		$tottime=120000;
-		$status="TO";
-		next;
-	    } elsif ($line =~ /.*user .*system (.*)elapsed .*CPU \(.*avgtext+.*avgdata (.*)maxresident\)k/) {
-		$timecmd=$1;
-		$tmem=$2;
-		if ($timecmd =~ /(\d+):(\d+)\.(\d+)/) {
-		    $timecmd = 60000*$1 + $2*1000 + $3;
-		}
-	    }
-	}
-	if ($ofp==1) {
-	    $status .= "_OF";	    
-	}
-	if ($oft==1) {
-	    $status .= "_OF";
-	}
-	close IN;
-	print "$model,PetriSpot,$cardp,$cardt,$carda,$ptime,$ttime,$constp,$nbp,$nbt,$tottime,$timecmd,$tmem,$status\n";
+    if ( $file =~ /petri(32|64|128)$/ ) {
+        my $model = $file;
+        my $tool = "PetriSpot$1";
+        $model =~ s/\.petri(32|64|128)//g;
+        my $status = "UNK";
+        my $ptime = -1, my $ttime = -1, my $constp = 0, my $nbp = 0, my $nbt = 0, my $tottime = -1, my $tmem = -1;
+        my $timecmd = -1;
+        my $colp = -1, my $colt = -1;
+        my $cardp = -1, my $cardt = -1, my $carda = -1;
+        open IN, "< $file";
+        my $ofp = 0, my $oft = 0;
+        while (my $line = <IN>) {
+            chomp $line;
+            if ($line =~ /Reduce places removed (\d+) places/) {
+                $constp = $1;
+                next;
+            } elsif ($line =~ /Computed (\d+) P\s+flows in (\d+) ms/) {
+                $nbp = $1;
+                $ptime = $2;
+                next;
+            } elsif ($line =~ /Computed (\d+) T\s+flows in (\d+) ms/) {
+                $nbt = $1;
+                $ttime = $2;
+                next;
+            } elsif ($line =~ /Invariants computation overflowed/) {
+                if ($ptime == -1) {
+                    $ofp = 1;
+                } else {
+                    $oft = 1;
+                }
+            } elsif ($line =~ /Parsed PT model containing (\d+) places and (\d+) transitions and (\d+) arcs in (\d+) ms/) {
+                $cardp = $1;
+                $cardt = $2;
+                $carda = $3;
+                # $parsetime = $4;
+            } elsif ($line =~ /Total runtime (\d+) ms/) {
+                $tottime = $1;
+                $status = "OK";
+                next;
+            } elsif ($line =~ /TIME LIMIT/) {
+                $tottime = 120000;
+                $status = "TO";
+                next;
+            } elsif ($line =~ /.*user .*system (.*)elapsed .*CPU \(.*avgtext+.*avgdata (.*)maxresident\)k/) {
+                $timecmd = $1;
+                $tmem = $2;
+                if ($timecmd =~ /(\d+):(\d+)\.(\d+)/) {
+                    $timecmd = 60000 * $1 + $2 * 1000 + $3;
+                }
+            }
+        }
+        if ($ofp == 1) {
+            $status .= "_OF";
+        }
+        if ($oft == 1) {
+            $status .= "_OF";
+        }
+        close IN;
+        print "$model,$tool,$cardp,$cardt,$carda,$ptime,$ttime,$constp,$nbp,$nbt,$tottime,$timecmd,$tmem,$status\n";
     }
-}   
+}
+  
 
 
 
