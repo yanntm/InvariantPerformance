0e1d937de3ad303f05df7bd9b9b2444bd71503ef
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2025-02-14 11:57:29 +0100

    reinstate ITS-tools parser

--- logs2csv2.pl
+++ logs2csv2.pl
@@ -108,3 +108,104 @@ foreach my $file (@files) {
         print "$model,$tool,$examination,$cardp,$cardt,$carda,$nbp,$nbt,$time_internal,$timecmd,$tmem,$status\n";
     }
 }
+
+
+
+my @files = <*its>;
+
+foreach my $file (@files) {
+    if ( $file =~ /its$/ ) {  
+        my $model = $file;
+        $model =~ s/\.its//g;
+        my $tool = "ItsTools";
+        my $status = "UNK";
+        my $ptime = -1, my $ttime = -1, my $nbp = -1, my $nbt = -1, my $tottime = -1, my $tmem = -1;
+        my $timecmd = -1;
+        my $cardp = -1, my $cardt = -1, my $carda = -1;
+        my $examination = "UNK";
+        my $ofp = 0, my $oft = 0;
+        
+        open IN, "< $file";
+        my $first_line = <IN>;
+        if ($first_line =~ /--Pflows/ and $first_line =~ /--Tflows/) {
+            $examination = "FLOWS";
+        } elsif ($first_line =~ /--Psemiflows/ and $first_line =~ /--Tsemiflows/) {
+            $examination = "SEMIFLOWS";
+        } elsif ($first_line =~ /--Pflows/) {
+            $examination = "PFLOWS";
+        } elsif ($first_line =~ /--Psemiflows/) {
+            $examination = "PSEMIFLOWS";
+        } elsif ($first_line =~ /--Tflows/) {
+            $examination = "TFLOWS";
+        } elsif ($first_line =~ /--Tsemiflows/) {
+            $examination = "TSEMIFLOWS";
+        }
+        # Reset file pointer to process all lines
+        seek(IN, 0, 0);
+        
+        while (my $line = <IN>) {
+            chomp $line;
+            if ($line =~ /Reduce places removed (\d+) places/) {
+                # Not output for ITS; just ignore
+                next;
+            } elsif ($line =~ /Computed (\d+) P\s+flows in (\d+) ms/) {
+                $nbp = $1;
+                $ptime = $2;
+                next;
+            } elsif ($line =~ /Computed (\d+) T\s+flows in (\d+) ms/) {
+                $nbt = $1;
+                $ttime = $2;
+                next;
+            } elsif ($line =~ /Invariants computation overflowed/) {
+                if ($ptime == -1) {
+                    $ofp = 1;
+                } else {
+                    $oft = 1;
+                }
+            } elsif ($line =~ /Parsed PT model containing (\d+) places and (\d+) transitions and (\d+) arcs in (\d+) ms/) {
+                $cardp = $1;
+                $cardt = $2;
+                $carda = $3;
+            } elsif ($line =~ /Unfolded HLPN to a Petri net with (\d+) places and (\d+) transitions (\d+) arcs in (\d+) ms/) {
+                $cardp = $1;
+                $cardt = $2;
+                $carda = $3;
+            } elsif ($line =~ /Total runtime (\d+) ms/) {
+                $tottime = $1;
+                $status = "OK";
+                next;
+            } elsif ($line =~ /TIME LIMIT/) {
+                $tottime = 120000;
+                $status = "TO";
+                next;
+            } elsif ($line =~ /.*user .*system (.*)elapsed .*CPU \(.*avgtext+.*avgdata (.*)maxresident\)k/) {
+                $timecmd = $1;
+                $tmem = $2;
+                if ($timecmd =~ /(\d+):(\d+)\.(\d+)/) {
+                    $timecmd = 60000*$1 + $2*1000 + $3;
+                }
+            }
+        }
+        close IN;
+        
+        if ($ofp || $oft) {
+            $status = "OF";
+            $nbp = -1;
+            $nbt = -1;
+        }
+        
+        # Determine TimeInternal: if both ptime and ttime are set and differ, report them as "ptime/ttime"
+        my $time_internal;
+        if ($ptime != -1 && $ttime != -1 && $ptime != $ttime) {
+            $time_internal = "$ptime/$ttime";
+        } elsif ($ptime != -1) {
+            $time_internal = $ptime;
+        } else {
+            $time_internal = $ttime;
+        }
+        
+        print "$model,$tool,$examination,$cardp,$cardt,$carda,$nbp,$nbt,$time_internal,$timecmd,$tmem,$status\n";
+    }
+}
+
+
