077023ff6b6792efcf5948f7a3228382f5020dbe
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2025-02-23 15:08:30 +0100

    convenience to fuse logs

---
+++ collectCSV.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+for i in logs_* ; 
+do
+  cd $i
+  type=$(echo $i | sed 's/logs_//')
+  ../log2csv2.pl > "../$type.csv"
+  cd ..
+done
+
+awk 'FNR==1 && NR!=1{next}1' ./*.csv > invar.csv
+
