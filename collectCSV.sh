acb0c315afb2e5379cf1b76debe9ae282ba438b0
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2025-02-25 12:31:26 +0100

    track files we use

--- collectCSV.sh
+++ collectCSV.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Array to track CSV basenames generated in this run
+csv_types=()
 
-for i in logs_* ; 
-do
-  cd $i
-  type=$(echo $i | sed 's/logs_//')
-  ../logs2csv2.pl > "../$type.csv"
-  cd ..
+# Process logs_* directories
+for i in logs_*; do
+  cd "$i" || exit 1
+  type=$(echo "$i" | sed 's/logs_//')
+  ../logs2csv2.pl > "../$type.csv"  # Writes to ../$type.csv
+  csv_types+=("$type.csv")          # Store just the basename (e.g., pflows.csv)
+  cd .. || exit 1
 done
 
-awk 'FNR==1 && NR!=1{next}1' ./*.csv > invar.csv
-
+# Merge only the CSVs generated in this run
+awk 'FNR==1 && NR!=1{next}1' "${csv_types[@]}" > invar.csv
