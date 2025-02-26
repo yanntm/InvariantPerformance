f2d4a891f2b0468a21918c98a44a92f1b56596fb
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2025-02-26 15:38:53 +0100

    run in //

--- collectCSV.sh
+++ collectCSV.sh
@@ -7,10 +7,13 @@ csv_types=()
 for i in logs_*; do
   cd "$i" || exit 1
   type=$(echo "$i" | sed 's/logs_//')
-  ../logs2csv2.pl > "../$type.csv"  # Writes to ../$type.csv
+  ../logs2csv2.pl > "../$type.csv"  & # Writes to ../$type.csv : Detached !
   csv_types+=("$type.csv")          # Store just the basename (e.g., pflows.csv)
   cd .. || exit 1
 done
+
+# Wait politely for all detached children to finish
+wait
 
 # Merge only the CSVs generated in this run
 awk 'FNR==1 && NR!=1{next}1' "${csv_types[@]}" > invar.csv
